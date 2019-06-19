aa08ae13bcba1bca705b5135f220df59d9c86f14
lisaveta256 <47987290+lisaveta256@users.noreply.github.com>
2019-06-19 18:15:25 +0300

    Add files via upload

---
+++ photo.js
@@ -0,0 +1,8 @@
+function InvoiceCntl($scope) {
+    $scope.qty = 331112233;
+    $scope.wunction=function(){
+    arr=$scope.qty1 = $scope.qty+"fgf";
+    newMass=arr.split("");
+    $scope.qty1="("+newMass[0]+newMass[1]+") "+newMass[2]+newMass[3]+newMass[4]+"-"+newMass[5]+newMass[6]+"-"+newMass[7]+newMass[8];
+  };
+}
